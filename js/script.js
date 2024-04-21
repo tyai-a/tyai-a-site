974f399b8a3c96033dbb221769c791cfe23cc050
Achi Takuya <achi@amr.co.jp>
2024-04-21 09:06:09 +0900

    目次の出力処理のリファクタリング

--- js/script.js
+++ js/script.js
@@ -15,59 +15,48 @@ document.addEventListener('DOMContentLoaded', function () {
 	/**
 	 * 目次生成
 	 */
-	var contentsList = document.getElementById('toc'); // 目次を追加する先(table of contents)
-	var div = document.createElement('div'); // 作成する目次のコンテナ要素
+	const tocElementClassName = ["inline-block","py-1","-ms-px","border-s-2","border-transparent","text-sm","text-slate-700","hover:border-slate-400","hover:text-slate-900","dark:text-slate-400","dark:hover:text-slate-300","hs-scrollspy-active:font-medium","hs-scrollspy-active:text-blue-600","dark:hs-scrollspy-active:text-blue-400"];
+	const tocIndent = {
+		"h2": "ps-4",
+		"h3": "ps-6",
+		"h4": "ps-8",
+		"h5": "ps-10",
+		"h6": "ps-12",
+	}
 
-	// .entry-content配下のh2、h3要素を全て取得する
-	var matches = document.querySelectorAll('#article-body h2, #article-body h3');
+	const contentsList = document.getElementById('toc');
+	const matches = document.querySelectorAll('#article-body h2, #article-body h3, #article-body h4, #article-body h5, #article-body h6');
 
 	// 取得した見出しタグ要素の数だけ以下の操作を繰り返す
 	matches.forEach(function (value, i) {
-		// 見出しタグ要素のidを取得し空の場合は内容をidにする
 		var id = value.id;
 		if(id === '') {
 			id = value.textContent;
 			value.id = id;
 		}
 
-		// 要素がh2タグの場合
-		if(value.tagName === 'H2') {
+		const generator = (indent) => {
 			var ul = document.createElement('ul');
 			var li = document.createElement('li');
 			var a = document.createElement('a')
 
-			// 追加する<ul><li><a>タイトル</a></li></ul>を準備する
 			a.innerHTML = value.textContent;
 			a.href = '#' + value.id;
-			a.classList.add("py-1","ps-4","-ms-px","border-s-2","border-transparent","text-sm","text-slate-700","hover:border-slate-400","hover:text-slate-900","dark:text-slate-400","dark:hover:text-slate-300","hs-scrollspy-active:font-medium","hs-scrollspy-active:text-blue-600","dark:hs-scrollspy-active:text-blue-400");
+			a.classList.add(...tocElementClassName);
+			a.classList.add(indent)
+			if(i === 0){
+				a.classList.add("active");
+			}
 			li.appendChild(a)
-			ul.appendChild(li);
-
-			// コンテナ要素である<div>の中に要素を追加する
-			div.appendChild(ul);
+			contentsList.appendChild(li);
 		}
 
-		// 要素がh3タグの場合
-		if(value.tagName === 'H3') {
-			var ul = document.createElement('ul');
-			var li = document.createElement('li');
-			var a = document.createElement('a');
+		if(value.tagName === 'H2') generator(tocIndent.h2)
+		if(value.tagName === 'H3') generator(tocIndent.h3)
+		if(value.tagName === 'H4') generator(tocIndent.h4)
+		if(value.tagName === 'H5') generator(tocIndent.h5)
+		if(value.tagName === 'H6') generator(tocIndent.h6)
 
-			// コンテナ要素である<div>の中から最後の<li>を取得する。
-			var lastUl = div.lastElementChild;
-			var lastLi = lastUl.lastElementChild;
-
-			// 追加する<ul><li><a>タイトル</a></li></ul>を準備する
-			a.innerHTML = value.textContent;
-			a.href = '#' + value.id;
-			li.appendChild(a)
-			ul.appendChild(li);
-
-			// 最後の<li>の中に要素を追加する
-			lastLi.appendChild(ul);
-		}
 	});
 
-	// 最後に画面にレンダリング
-	contentsList.appendChild(div);
 });
