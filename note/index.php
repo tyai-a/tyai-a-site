a85efc33ca57b0c7294a7b0ff6dd3f4454ebc05a
Achi Takuya <achi@amr.co.jp>
2024-04-21 09:45:10 +0900

    カードコンポーネントを1つだけに減らし、デザインを更新
    - 記事名の文字サイズを少し大きく
    - カテゴリのデザインを変更
    - 作成日時を追加
    - hrefのURLを相対に

--- note/index.php
+++ note/index.php
@@ -39,118 +39,24 @@
 
 <div class="flex flex-wrap -m-4">
 
-<a href="https://tyai-a.site/note/fix_500error_cgi_bin_to_html/" class="p-4 md:w-1/2 lg:w-1/3">
+<a href="ubuntu_22044_mount_storage/" class="p-4 md:w-1/2 lg:w-1/3">
 <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white dark:bg-gray-700">
-<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/ubuntu.png" alt="blog">
+<img class="lg:h-64 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/ubuntu.png" alt="blog">
 <div class="p-6 border-t-2 border-gray-200 border-opacity-60">
-<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Ubuntu</h2>
-<h1 class="title-font text-lg font-medium text-gray-900 dark:text-white mb-3">Ubuntu 22.04LTS で /cgi-bin/ 下のHTMLファイルを開くと500エラーとなる問題を解決</h1>
-<p class="leading-relaxed dark:text-white mb-3">
+<div class="flex gap-2 mb-3">
+  <span class="py-1 px-2 text-sm font-medium rounded-md border border-gray-400 bg-gray-100 text-gray-600 dark:bg-neutral-800/25 dark:border-neutral-500 dark:text-neutral-300">Ubuntu</span>
+</div>
+<p class="text-xl font-medium text-gray-900 dark:text-white mb-3">Ubuntu 22.04LTS で /cgi-bin/ 下のHTMLファイルを開くと500エラーとなる問題を解決</p>
+<p class="leading-relaxed dark:text-gray-400 mb-3">
 
 解決したい問題 /cgi-bin/下のHTMLファイルを開くと500 InternalServerE...
 </p>
+<div class="flex flex-col justify-end sm:flex-row gap-1 sm:gap-3 text-sm font-normal text-gray-600 dark:text-gray-400 mt-3">
+  <time class="flex items-center gap-2">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock-9 w-4"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 7.5 12"></polyline></svg>
+  	公開日: 2024年4月10日 03:51
+  </time>
 </div>
-</div>
-</a>
-
-
-
-
-<a href="https://tyai-a.site/note/nextcloud_error_client_not_https/" class="p-4 md:w-1/2 lg:w-1/3">
-<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white dark:bg-gray-700">
-<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/nextcloud.png" alt="blog">
-<div class="p-6 border-t-2 border-gray-200 border-opacity-60">
-<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">NextCloud</h2>
-<h1 class="title-font text-lg font-medium text-gray-900 dark:text-white mb-3">NextCloud のクライアントソフトのエラーを解消</h1>
-<p class="leading-relaxed dark:text-white mb-3">
-
-NextCloud のクライアントソフトをインストールし、ログインURLを設定した際に以下のようなエ...
-</p>
-</div>
-</div>
-</a>
-
-
-
-
-<a href="https://tyai-a.site/note/problem_nextcloud_setup/" class="p-4 md:w-1/2 lg:w-1/3">
-<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white dark:bg-gray-700">
-<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/nextcloud.png" alt="blog">
-<div class="p-6 border-t-2 border-gray-200 border-opacity-60">
-<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">NextCloud</h2>
-<h1 class="title-font text-lg font-medium text-gray-900 dark:text-white mb-3">NextCloud のインストールで躓いた</h1>
-<p class="leading-relaxed dark:text-white mb-3">
-
-自宅サーバーに NextCloud をインストールしたのですが、 初回セットアップ時に必ず必要となる...
-</p>
-</div>
-</div>
-</a>
-
-
-
-
-<a href="https://tyai-a.site/note/check_php_modules/" class="p-4 md:w-1/2 lg:w-1/3">
-<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white dark:bg-gray-700">
-<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/php.png" alt="blog">
-<div class="p-6 border-t-2 border-gray-200 border-opacity-60">
-<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">PHP</h2>
-<h1 class="title-font text-lg font-medium text-gray-900 dark:text-white mb-3">インストールされているPHPモジュールを確認する</h1>
-<p class="leading-relaxed dark:text-white mb-3">
-
-インストールされているPHPモジュールを確認してみます。 コマンドラインで php -m と入力すれ...
-</p>
-</div>
-</div>
-</a>
-
-
-
-
-<a href="https://tyai-a.site/note/ubuntu_22044_mount_storage/" class="p-4 md:w-1/2 lg:w-1/3">
-<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white dark:bg-gray-700">
-<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/ubuntu.png" alt="blog">
-<div class="p-6 border-t-2 border-gray-200 border-opacity-60">
-<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Ubuntu</h2>
-<h1 class="title-font text-lg font-medium text-gray-900 dark:text-white mb-3">Ubuntu 22.04.4 で増設したSSDを使用する</h1>
-<p class="leading-relaxed dark:text-white mb-3">
-
-Ubuntu 22.04.4 にSATA接続のSSDを増設しました。 データを保存できるストレージと...
-</p>
-</div>
-</div>
-</a>
-
-
-
-
-<a href="https://tyai-a.site/note/basic_commnad_mysql/" class="p-4 md:w-1/2 lg:w-1/3">
-<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white dark:bg-gray-700">
-<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/mysql.png" alt="blog">
-<div class="p-6 border-t-2 border-gray-200 border-opacity-60">
-<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">MySQL</h2>
-<h1 class="title-font text-lg font-medium text-gray-900 dark:text-white mb-3">MySQLの基礎的なコマンド集</h1>
-<p class="leading-relaxed dark:text-white mb-3">
-
-操作方法を忘れることが多いので記事としてまとめておき、あとで見返せるようにしておきます。 文末のセミ...
-</p>
-</div>
-</div>
-</a>
-
-
-
-
-<a href="https://tyai-a.site/note/tailwind_css_prose/" class="p-4 md:w-1/2 lg:w-1/3">
-<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white dark:bg-gray-700">
-<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/img/note/thumbnail/tailwind_css.png" alt="blog">
-<div class="p-6 border-t-2 border-gray-200 border-opacity-60">
-<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Tailwind CSS</h2>
-<h1 class="title-font text-lg font-medium text-gray-900 dark:text-white mb-3">Tailwind CSS の proseクラス で記事の見た目を整える</h1>
-<p class="leading-relaxed dark:text-white mb-3">
-
-Tailwind CSS に @tailwindcss/typography という公式プラグインを...
-</p>
 </div>
 </div>
 </a>
